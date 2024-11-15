72201d8124b276c7983b2bb108718d9a38ef2866
Zac Wong <zacwong999@gmail.com>
2024-11-15 11:33:43 +0800

    add routes for hk modules

--- pages/routes.py
+++ pages/routes.py
@@ -30,3 +30,27 @@ def subpage3():
         answers = request.form.to_dict()
         return jsonify(answers)
     return render_template('module_3.html')
+
+# HK Subpage 1
+@pages.route('/hk_module_1', methods=['GET', 'POST'])
+def hksubpage1():
+    if request.method == 'POST':
+        answers = request.form.to_dict()
+        return jsonify(answers)
+    return render_template('hk_module_1.html')
+
+# HK Subpage 2
+@pages.route('/hk_module_2', methods=['GET', 'POST'])
+def hksubpage2():
+    if request.method == 'POST':
+        answers = request.form.to_dict()
+        return jsonify(answers)
+    return render_template('hk_module_2.html')
+
+# HK Subpage 3
+@pages.route('/hk_module_3', methods=['GET', 'POST'])
+def hksubpage3():
+    if request.method == 'POST':
+        answers = request.form.to_dict()
+        return jsonify(answers)
+    return render_template('hk_module_3.html')
